3355bfa4bca3f360713d8de5b7fff671ed332342
coosmiin <coosmiin@yahoo.com>
2020-01-04 12:13:18 +0200

    Small change on trading console output

--- src/BVBTrading.Console/Program.cs
+++ src/BVBTrading.Console/Program.cs
@@ -5,26 +5,26 @@ using Trading.BVBScraper;
 
 namespace BVBTrading.Console
 {
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-   			var scraper = new StockScraper();
-            var betStocks = await scraper.ScrapeBETComposition();
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var scraper = new StockScraper();
+			var betStocks = await scraper.ScrapeBETComposition();
 
-            var stockPrices = betStocks.ToDictionary(s => s.Symbol, s => s.Price);
-            var targetWeights = betStocks.ToDictionary(s => s.Symbol, s => s.Weight);
+			var stockPrices = betStocks.ToDictionary(s => s.Symbol, s => s.Price);
+			var targetWeights = betStocks.ToDictionary(s => s.Symbol, s => s.Weight);
 
-            var portfolio = new PortfolioBuilder()
-                .UsePrices(stockPrices)
-                .UseWeights(targetWeights)
-                .UseAmount(2000)
-                .Build();
+			var portfolio = new PortfolioBuilder()
+				.UsePrices(stockPrices)
+				.UseWeights(targetWeights)
+				.UseAmount(2000)
+				.Build();
 
 			foreach (var stock in portfolio)
 			{
-				System.Console.WriteLine($"{stock.Symbol} ({stock.Price})\t {stock.Count}\t {stock.Weight} ({targetWeights[stock.Symbol]})");
-			}            
-        }
-    }
+				System.Console.WriteLine($"{stock.Symbol} ({stock.Price})\t {stock.Count} ({stock.Count * stock.Price})\t {stock.Weight} ({targetWeights[stock.Symbol]})");
+			}
+		}
+	}
 }

1e5b384b53bd5f05a88f8641805d0039d1af9a06
coosmiin <coosmiin@users.noreply.github.com>
2020-03-08 16:46:59 +0200

    FIxed min-weight value calculation (#26)
    #20

---
+++ src/Investments.Advisor/Providers/StaticDataBVBDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using Investments.Advisor.Models;
+using Investments.Advisor.Providers.StaticData;
+using Investments.Domain.Stocks;
+using Investments.Utils.Serialization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Investments.Advisor.Providers
+{
+	public class StaticDataBVBDataProvider : IBVBDataProvider
+	{
+		public Task<Stock[]> GetBETStocksAsync()
+		{
+			var indexStocks = JsonSerializerHelper.Deserialize<BETStock[]>(Resources.bet_index);
+
+			return Task.FromResult(indexStocks.Select(s => new Stock(s.Symbol) { Price = s.Price, Weight = s.Weight }).ToArray());
+		}
+	}
+}

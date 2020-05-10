475d8c0a45884061f37bfcce6c7506e951a6a4b3
coosmiin <coosmiin@users.noreply.github.com>
2020-05-10 14:28:21 +0300

    Fixed serialization issue when submitting orders (#51)
    #50

--- src/Trading.Functions/Orchestrator/OrchestratorFunctions.cs
+++ src/Trading.Functions/Orchestrator/OrchestratorFunctions.cs
@@ -16,14 +16,14 @@ namespace Trading.Functions.Orchestrator
 		}
 
 		[FunctionName(nameof(StartTradingSession))]
-		public async void StartTradingSession(
+		public void StartTradingSession(
 			[HttpTrigger(AuthorizationLevel.Function, "get", Route = "startTradingSession")] HttpRequest request)
 		{
-			await _orchestrator.Run();
+			_orchestrator.Run();
 		}
 
 		[FunctionName(nameof(TriggerTradingSession))]
-		public async void TriggerTradingSession([TimerTrigger("0 0 12 8 * *")] TimerInfo timer, ILogger logger)
+		public void TriggerTradingSession([TimerTrigger("0 0 12 8 * *")] TimerInfo timer, ILogger logger)
 		{
 			if (timer.IsPastDue)
 			{
@@ -31,7 +31,7 @@ namespace Trading.Functions.Orchestrator
 				return;
 			}
 
-			await _orchestrator.Run();
+			_orchestrator.Run();
 		}
 	}
 }
